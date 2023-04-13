2cbf5d06d2bc09f9a6eadf895dfca30c37366725
David Berger <127954197+David2945@users.noreply.github.com>
2023-04-13 12:14:05 +0200

    Add files via upload

--- PSU_LV3/zad1.py
+++ PSU_LV3/zad1.py
@@ -4,18 +4,19 @@ import matplotlib.pyplot as plt
 
 mtcars = pd.read_csv('mtcars.csv')
 print(mtcars)
-#1. Kojih 5 automobila ima najveću potrošnju
+#1. Kojih 5 automobila ima najveću potrošnju? (koristite funkciju sort)
 print(mtcars.sort_values('mpg')[0:5])
 #2. Koja 3 automobila s 8 cilindara imaju najmanju potrosnju
 print(mtcars.sort_values('mpg', ascending = False)[(mtcars.cyl == 8)][0:3])
-#3.Kolika je srednja potrošnja automobila sa 6 cilindara
+#3. Kolika je srednja potrošnja automobila sa 6 cilindara?
 print(mtcars[(mtcars.cyl == 6)]['mpg'].mean())
-#4.Kolika je srednja potrošnja automobila s 4 cilindara mase 2000-2200
+#4. Kolika je srednja potrošnja automobila s 4 cilindra mase između 2000 i 2200 lbs?
 print(mtcars[(mtcars.cyl == 4) & (mtcars.wt >= 2.0) & ( mtcars.wt <= 2.2)]['mpg'].mean())
-#5.Koliko je automobila s ručnim, a koliko s automatskim mjenjačem
+#5. Koliko je automobila s ručnim, a koliko s automatskim mjenjačem u ovom skupu podataka?
 print(mtcars[(mtcars.am == 1)].count().car)
 print(mtcars[(mtcars.am == 0)].count().car)
-#6.Koliko je automobila s automatskim mjenjačem i snagom preko 100 hp
+#6. Koliko je automobila s automatskim mjenjačem i snagom preko 100 konjskih snaga?
 print(mtcars[(mtcars.am == 1) & (mtcars.hp > 100)].count().car)
-#7.Kolika je masa svakog automobila u kg
+#7. Kolika je masa svakog automobila u kilogramima?
 print(mtcars.wt * 0.45 * 1000)
+
--- PSU_LV3/zad2.py
+++ PSU_LV3/zad2.py
@@ -3,3 +3,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 mtcars = pd.read_csv('mtcars.csv')
+print(mtcars)
+
+'''''
+#1. Pomoću barplot-a prikažite na istoj slici potrošnju automobila s 4, 6 i 8 cilindara.
+plt.figure(figsize = (20, 5))
+plt.bar(mtcars['mpg'], mtcars['cyl'], color='green', width = 0.2)
+plt.xlabel("Potrosnja - MPG")
+plt.ylabel("Broj cilindara")
+plt.title("1. Pomoću barplot-a prikažite na istoj slici potrošnju automobila s 4, 6 i 8 cilindara")
+plt.show()
+'''''
+
+'''''
+#2. Pomoću boxplot-a prikažite na istoj slici distribuciju težine automobila s 4, 6 i 8 cilindara.
+plt.figure(figsize = (10, 5))
+plt.boxplot(mtcars)
+plt.show()
+'''''
+
+#3. Pomoću odgovarajućeg grafa pokušajte odgovoriti na pitanje imaju li automobili s ručnim mjenjačem veću
+# potrošnju od automobila s automatskim mjenjačem?
+plt.figure(figsize = (20, 5))
+plt.scatter(mtcars['mpg'], mtcars['am'], c='blue')
+plt.xlabel("Potrosnja - MPG")
+plt.ylabel("Mjenač: 0 - automatic, 1 - manual")
+plt.title("3. Pomoću odgovarajućeg grafa pokušajte odgovoriti na pitanje imaju li automobili s ručnim mjenjačem veću potrošnju od automobila s automatskim mjenjačem?")
+plt.show()
+
+#4. Prikažite na istoj slici odnos ubrzanja i snage automobila za automobile s ručnim odnosno automatskim mjenjačem.
+plt.figure(figsize = (20, 5))
+plt.scatter(mtcars[['hp', 'qsec']], mtcars['am'], c='red')
+plt.xlabel("Snaga i ubrzanje")
+plt.ylabel("Mjenač: 0 - automatic, 1 - manual")
+plt.title("4. Prikažite na istoj slici odnos ubrzanja i snage automobila za automobile s ručnim odnosno automatskim mjenjačem.")
+plt.show()
